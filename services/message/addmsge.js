29f1c588cac540539a6ce67f980c80a36c65a9f6
Mohamed Ehab <73292867+mody19765@users.noreply.github.com>
2023-07-27 22:27:30 +0300

    first commit

--- services/message/addmsge.js
+++
@@ -1,9 +0,0 @@
-import { MessagesModel } from "../../models/message.js"
-
-
-const addmesg = async (req, res) => {
-   const { message, UserID } = req.body
-   const messages = await MessagesModel.insertMany({ message, UserID })
-   res.json({ message: "mesg send" })
-}
-export { addmesg }
--- services/message/getmsge.js
+++
@@ -1,8 +0,0 @@
-import { MessagesModel } from "../../models/message.js";
-
-const getmsg=async(req,res)=>{
-    const Messages= await MessagesModel.find({UserID:req.id},{_id:0,message:1})
-    res.json({message:"succes",Messages})
-   
-}
-export{getmsg}
